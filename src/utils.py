4394eab6a3783e946ec09a5e3975ef55b0092f4f
Offisong Emmanuel <57522480+Techtacles@users.noreply.github.com>
2025-05-29 12:34:20 +0000

    adding local cache and fixing docker run command on readme

--- src/utils.py
+++ src/utils.py
@@ -199,7 +199,15 @@ def load_selected_model():
     try:
         get_latest_object_key = get_latest_model_artifact_from_bucket(s3_client, BUCKET_NAME).get("Key")
         print(f"Key is {get_latest_object_key}")
-        download_artifact_from_bucket(s3_client, BUCKET_NAME, get_latest_object_key)
+        current_working_directory = os.getcwd()
+        print(f"Current working directory is {current_working_directory}")
+        path_to_check = f"{current_working_directory}/{get_latest_object_key}"
+        if os.path.isfile(path_to_check):
+            print(f"Model found locally... using model {path_to_check}")
+            model_path = f"model_artefacts/{get_latest_object_key}"
+        else:
+            print(f"Downloading artifact {get_latest_object_key} from bucket {BUCKET_NAME} ")
+            download_artifact_from_bucket(s3_client, BUCKET_NAME, get_latest_object_key)
 
         model_path = f"model_artefacts/{get_latest_object_key}"
         model = load_model(model_path)
